437e3335956795fd85bd21b7be72c4e2cb1a3bac
GabrielP95 <plazagabriel1995@gmail.com>
2023-05-17 15:49:14 -0300

    Creacion del modelo Empleado

--- src/biblioteca/models.py
+++ src/biblioteca/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    numero_legajo = models.PositiveIntegerField()
+    activo = models.BooleanField(default=True)

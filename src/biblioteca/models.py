4ec4541d75b0f56a0e8e1a8d831feae2c9405893
GabrielP95 <plazagabriel1995@gmail.com>
2023-05-17 19:09:35 -0300

    Creacion de la app Empleado para separarlo de la aplicacion principal

--- src/biblioteca/models.py
+++ src/biblioteca/models.py
@@ -1,8 +1,4 @@
 from django.db import models
 
 # Create your models here.
-class Empleado(models.Model):
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    numero_legajo = models.PositiveIntegerField()
-    activo = models.BooleanField(default=True)
+

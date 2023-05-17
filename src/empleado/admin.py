548d4dead2b5f852e606e9d98ac4d04e901a6139
GabrielP95 <plazagabriel1995@gmail.com>
2023-05-17 20:03:54 -0300

    Registré el modelo Empleado en el Admin

--- src/empleado/admin.py
+++ src/empleado/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from models import Empleado
 
 # Register your models here.
-admin.site.register(Empleado)
+
+class EmpleadoAdmin(admin.ModelAdmin):
+	# Me mostrará una tabla con la siguiente forma:
+	# apellido | nombre | numero_legajo | activo
+	list_display = ['apellido', 'nombre', 'numero_legajo','activo']
+	# agrega opciones de busqueda por nombre o apellido
+	search_fields = ['nombre', 'apellido']
+	# agrega un filtro por el campo 'activo'
+	list_filter = ['activo']
+	
+admin.site.register(Empleado, EmpleadoAdmin)

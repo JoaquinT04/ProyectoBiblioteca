69c4ce5020ae76cb8e8df736adfdad60c9092369
“Ramiro <ra31lopez@gmail.com>
2023-05-16 22:12:09 -0300

    ADD + Agregado modelo Socio

--- src/biblioteca/models.py
+++ src/biblioteca/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+""" Creacion de la entidad Socio cuenta con:
+nombre = nombre del Socio (tipo Str)
+apellido = apellido del Socio (tipo Str)
+fecha_nacimiento = la fecha de nacimiento del Socio (tipo Date)
+activo = si el socio se encuentra activo o no (tipo Bool)
+ """
+class Socio(models.Model):
+    nombre = models.CharField(max_length= 30)
+    apellido = models.CharField(max_length= 30)
+    fecha_nacimiento = models.DateField()
+    activo = models.BooleanField(default= True)

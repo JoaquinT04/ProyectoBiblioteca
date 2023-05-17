62456cad83199283c8cf2740915052b58afa98a8
xalvlax <sergiosalvatierra86@gmail.com>
2023-05-17 00:50:32 -0300

    Se creo modelo PrestamoLibro. A la espera de los modelos Socio, Empleado y Libro para migrar a base de datos

--- src/biblioteca/models.py
+++ src/biblioteca/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from socio.models import Socio
+from empleado.models import Empleado
+from libro.models import Libro
 
 # Create your models here.
+class PrestamoLibro(models.Model):
+    fecha_prestamos: models.DateField(auto_now_add=True)
+    fecha_devolucion: models.DateField()
+    socio: models.ForeignKey(Socio, on_delete=CASCADE)
+    empleado: models.ForeignKey(Empleado, on_delete=CASCADE)
+    libro: models.ForeignKey(Libro, on_delete=CASCADE)
+
+#Generar el archivo migration (command makemigrations)
+
+#Aplicar el archivo migration (command migrate)

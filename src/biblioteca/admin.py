0dce7b2f0e3596a3704fe319227ed707071119a6
xalvlax <sergiosalvatierra86@gmail.com>
2023-05-18 18:09:28 -0300

    ADD Se corrigieron atributos en admin

--- src/biblioteca/admin.py
+++ src/biblioteca/admin.py
@@ -7,8 +7,8 @@ from .models import PrestamoLibro
 # Se define elatributo list_display para mostrar campos por columna. 
 # Se agrega la búsqueda por socio, empleado y libro.
 class PrestamoLibroAdmin(admin.ModelAdmin):
-    ordering = ('fecha_devolucion')
-    list_display = ('fecha_prestamos', 'fecha_devolucion', 'socio', 'empleado', 'libro')
-    search_fields = ('socio', 'empleado', 'libro')
+    ordering = ['fecha_devolucion']
+    list_display = ['fecha_prestamos', 'fecha_devolucion', 'socio', 'empleado', 'libro']
+    search_fields = ['socio', 'empleado', 'libro']
 
 admin.site.register(PrestamoLibro, PrestamoLibroAdmin)

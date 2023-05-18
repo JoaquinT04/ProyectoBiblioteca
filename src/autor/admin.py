4afbee5efc9ca3f4d55f1924c02635e652bfc732
Dan <dantealbertomartinez@gmail.com>
2023-05-18 16:40:41 -0300

    en admin se cambio () por []

--- src/autor/admin.py
+++ src/autor/admin.py
@@ -9,8 +9,8 @@ class AutorAdmin(admin.ModelAdmin):
         Se Agrega opciones de búsqueda por nombre o apellido (list_search = search_fields)
         Se agrega la opción de filtrado por el field activo y nacionalidad (list_filter) """
     
-    list_display = ("nombre","apellido","nacionalidad","activo")
-    list_filter = ("activo", "nacionalidad")
-    search_fields = ('nombre', 'apellido')
+    list_display = ["nombre","apellido","nacionalidad","activo"]
+    list_filter = ["activo", "nacionalidad"]
+    search_fields = ['nombre', 'apellido']
 
 admin.site.register(Autor, AutorAdmin) # Registrar el modelo en el archivo admin.py

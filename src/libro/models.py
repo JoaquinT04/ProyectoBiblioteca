e082e55b42524750a20b93271ab71b4a040be5d8
Dan <dantealbertomartinez@gmail.com>
2023-05-19 04:45:01 -0300

    se corrigio la confi jinja

--- src/libro/models.py
+++ src/libro/models.py
@@ -9,15 +9,15 @@ class Libro(models.Model):
     # isbn del libro (El isbn es un estandard número de 13 cifras que identifica a cada libro en el mundo)
     isbn = models.PositiveIntegerField()
     # aqui estara la información del autor que se encuentra en su respectivo modelo
-    autor =  models.ForeignKey(Autor)
+    autor =  models.ForeignKey(Autor, on_delete= models.CASCADE)
     # activo hara referencia asi el libro esta disponible o no, por defecto a la hora de que se cree un libro estara disponible
     activo: models.BooleanField(default=True)
 
     def __str__(self) -> str:
-      return f"""titulo: {this.titulo}
-                 descripcion: {this.descripcion}
-                 isbn: {this.isbn}
-                 autor: {this.autor}
+      return f"""titulo: {self.titulo}
+                 descripcion: {self.descripcion}
+                 isbn: {self.isbn}
+                 autor: {self.autor}
                  """
     class Meta:
       ordering = [

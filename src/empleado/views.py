8f7e8058d030b46ccd6f48c64a980bfc75b5c991
xalvlax <sergiosalvatierra86@gmail.com>
2023-05-18 23:05:19 -0300

    ADD Se creo views desactivar_empleado

--- src/empleado/views.py
+++ src/empleado/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Empleado
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+def desactivar_empleado(request, id):
+    empleado = get_object_or_404(Empleado, id=id)
+    empleado.activo = False
+    empleado.save()
+    
+    mensaje = f'Empleado: {empleado.apellido}, {empleado.nombre}\n'
+    mensaje += f'Legajo: {empleado.numero_legajo}\n'
+    mensaje += f'DESACTIVADO con exito!!'
+
+    return mensaje
+

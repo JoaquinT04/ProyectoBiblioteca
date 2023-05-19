0323be8fb11ae645c2ab5cae5b6df9cc9dcc4ea5
xalvlax <sergiosalvatierra86@gmail.com>
2023-05-18 23:08:40 -0300

    ADD Se creo urls en app empleado

---
+++ src/empleado/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import desactivar_empleado
+
+app_name = 'empleados'
+
+urlpatterns = [
+    path('desactivar/<int: id>', desactivar_empleado, name= 'desactivar_empleado'),
+]

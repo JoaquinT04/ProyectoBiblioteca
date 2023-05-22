1e94a465b5a0cb52ef2febbd4cc24173acda36d2
“Ramiro <ra31lopez@gmail.com>
2023-05-22 18:32:29 -0300

    Add+ View para activar registros de Autores Agregado la url correspondiente (/autores/activar/<int: id>)

--- src/autor/urls.py
+++ src/autor/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import (
     EditarAutor,
+    activar_autor
     )
 
 app_name = 'autores'
 
 urlpatterns = [
     path('modificar/<int:id>/',EditarAutor.as_view(), name='EditarAutor'),
+    path('activar/<int:id>/',activar_autor, name='activar_autor'),
     
 ]
 
--- src/autor/views.py
+++ src/autor/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Autor
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from .forms import AutorForm
@@ -7,3 +7,14 @@ class EditarAutor(UpdateView):
     model = Autor
     form_class = AutorForm
     template_name = "autores/editar.html"
+
+def activar_autor(request, id):
+    autor = get_object_or_404(Autor, id=id)
+    autor.activo = True
+    autor.save()
+    
+    mensaje = f'Autor: {autor.apellido}, {autor.nombre}\n'
+    mensaje += f'Nacionalidad: {autor.nacionalidad}\n'
+    mensaje += f'ACTIVADO con exito!!'
+
+    return mensaje

6e4cd0675186d2bd2390e76f0efd8840bd6250c0
Joaquin Tejerina <joaquintejerina19@hotmail.com>
2023-05-22 16:48:19 -0300

    Creada la view para desactivar un autor

--- src/autor/urls.py
+++ src/autor/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import (
     EditarAutor,
+    desactivar_autor,
     )
 
 app_name = 'autores'
 
 urlpatterns = [
     path('modificar/<int:id>/',EditarAutor.as_view(), name='EditarAutor'),
-    
+    path('desactivar/<int:id>/',desactivar_autor, name='desactivar_autor'),
 ]
 
--- src/autor/views.py
+++ src/autor/views.py
@@ -7,3 +7,15 @@ class EditarAutor(UpdateView):
     model = Autor
     form_class = AutorForm
     template_name = "autores/editar.html"
+
+
+def desactivar_autor(request, id):
+    autor = get_object_or_404(Autor, id=id)
+    autor.activo = False
+    autor.save()
+    
+    mensaje = f'Autor: {autor.apellido}, {autor.nombre}\n'
+    mensaje += f'Nacionalidad: {autor.nacionalidad}\n'
+    mensaje += f'DESACTIVADO con exito!!'
+
+    return mensaje

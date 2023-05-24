fcd06c97c531fde793e2310db7cda5160f613045
xalvlax <sergiosalvatierra86@gmail.com>
2023-05-24 02:06:35 -0300

    ADD: Se creo views activar_socio

--- src/socio/views.py
+++ src/socio/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from socio.models import Socio
 
 # Create your views here.
+def activar_socio(request, id):
+    socio = get_object_or_404(Socio, id=id)
+    socio.activo = True
+    socio.save()
+    
+    mensaje = f'Socio: {socio.apellido}, {socio.nombre}\n'
+    mensaje += f'Legajo: {socio.fecha_nacimiento}\n'
+    mensaje += f'ACTIVADO con exito!!'
+
+    return mensaje

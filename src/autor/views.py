c890a76c6ea48a3e04416c801e0fac5e3a453215
xalvlax <sergiosalvatierra86@gmail.com>
2023-05-24 01:14:22 -0300

    ADD: Se creo views ListarAutores

--- src/autor/views.py
+++ src/autor/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Autor
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from .forms import AutorForm
 
 # Create your views here.
+class EditarAutor(UpdateView):
+    model = Autor
+    form_class = AutorForm
+    template_name = "autores/editar.html"
+
+class ListarAutores(ListView):
+	model = Autor
+	template_name = "autores/listar.html"
+
+
+def desactivar_autor(request, id):
+    autor = get_object_or_404(Autor, id=id)
+    autor.activo = False
+    autor.save()
+    
+    mensaje = f'Autor: {autor.apellido}, {autor.nombre}\n'
+    mensaje += f'Nacionalidad: {autor.nacionalidad}\n'
+    mensaje += f'DESACTIVADO con exito!!'
+
+    return mensaje

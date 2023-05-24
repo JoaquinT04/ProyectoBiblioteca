f9586633f1d464531cb01627a671fa39b6a579ef
Dan <dantealbertomartinez@gmail.com>
2023-05-23 23:21:31 -0300

    Se Link funcionalidades de autor-falta revision

--- src/autor/models.py
+++ src/autor/models.py
@@ -7,3 +7,12 @@ class Autor(models.Model):
     apellido = models.CharField(max_length=50)
     nacionalidad = models.CharField(max_length=30)
     activo = models.BooleanField(default = True)
+
+    class Meta():
+        ordering = ["apellido","nombre"]
+    
+    def __str__(self):
+        return f"""nombre: {self.nombre} 
+                   apellido: {self.apellido}
+                   nacionalidad: {self.nacionalidad}
+                   activo: {self.activo}"""

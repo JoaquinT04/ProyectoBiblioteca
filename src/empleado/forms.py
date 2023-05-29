e0dd89e882c88ca0a073c4f305171e6d31515cf7
Joaquin Tejerina <joaquintejerina19@hotmail.com>
2023-05-29 19:45:16 -0300

    importando forms en forms.py de empleado

--- src/empleado/forms.py
+++ src/empleado/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from .models import Empleado
 from .validators import nombreValidator
+from django import forms
 class EmpleadoForm(ModelForm):
 		nombre  = forms.CharField(min_length=3,max_length=50, validators=[nombreValidator])
 		apellido = forms.CharField(min_length=3,max_length=50, validators=[nombreValidator])

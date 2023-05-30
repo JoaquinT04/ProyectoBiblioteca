2e365e4b48fdde872be4cb6c91eb6765fa5e2860
Joaquin Tejerina <joaquintejerina19@hotmail.com>
2023-05-30 18:53:18 -0300

    View para Listar Prestamo Libro

--- src/biblioteca/views.py
+++ src/biblioteca/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
-
+from .models import PrestamoLibro
 # Create your views here.
-
+class ListarPrestamoLibro(ListView):
+	model = PrestamoLibro
+	template_name = "prestamoLibro/listar.html"

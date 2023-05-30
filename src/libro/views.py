d960f11f94595431fded04459f887eeffd2c61bb
xalvlax <sergiosalvatierra86@gmail.com>
2023-05-30 00:27:46 -0300

    ADD: Se crea views para crear registro de nuevo libro CrearLibro

--- src/libro/views.py
+++ src/libro/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Libro
+from .forms import LibroForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
+class CrearLibro(CreateView):
+    model = Libro
+    form_class = LibroForm
+    template_name = "libros/crear.html"
+    success_url = reverse_lazy('libros:ListarLibros') 

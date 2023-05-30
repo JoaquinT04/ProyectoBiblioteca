0f0c79c2aa2a96f5bcc0757253ec3839369136f2
xalvlax <sergiosalvatierra86@gmail.com>
2023-05-29 21:32:42 -0300

    Se corrigen errores en merge anterior

--- src/empleado/forms.py
+++ src/empleado/forms.py
@@ -1,14 +1,11 @@
 from django import forms
+from django.forms import ModelForm
 from .models import Empleado
 from .validators import nombreValidator
-<<<<<<< HEAD
-class EmpleadoForm(forms.ModelForm):
-=======
-from django import forms
+
 class EmpleadoForm(ModelForm):
->>>>>>> develop
-		nombre  = forms.CharField(min_length=3,max_length=50, validators=[nombreValidator])
-		apellido = forms.CharField(min_length=3,max_length=50, validators=[nombreValidator])
-		class Meta:
-				model = Empleado
-				fields = ["apellido","nombre","numero_legajo"]
+	nombre  = forms.CharField(min_length=3,max_length=50, validators=[nombreValidator])
+	apellido = forms.CharField(min_length=3,max_length=50, validators=[nombreValidator])
+	class Meta:
+			model = Empleado
+			fields = ["apellido","nombre","numero_legajo"]

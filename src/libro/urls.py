4cebcb364ca9c2518815b7e61c9ebb5e6c4350da
xalvlax <sergiosalvatierra86@gmail.com>
2023-05-31 20:27:51 -0300

    FIX: Se corrige listar.html y editar.html para que corra el servidor

--- src/libro/urls.py
+++ src/libro/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (
     CrearLibro,
     EditarLibro,
-    ListarLibro,
+    ListarLibros,
 		desactivar_libro,
     activar_libro,
 )
@@ -12,7 +12,7 @@ app_name = 'libros'
 urlpatterns = [
     path('nuevo/', CrearLibro.as_view(), name= 'CrearLibro' ),
     path('modificar/<int:pk>/',EditarLibro.as_view(), name='EditarLibro'),
-    path('listado/',ListarLibro.as_view(), name= 'ListarLibros' ),
+    path('listar/',ListarLibros.as_view(), name= 'ListarLibros' ),
     path('activar/<int:id>/', activar_libro, name= 'activar_libro'),
 		path('desactivar/<int:id>/', desactivar_libro, name= 'desactivar_libro'),
 ]

a45c7d803278b76e3e8275596f7d5d5cf5dcee4c
xalvlax <sergiosalvatierra86@gmail.com>
2023-05-31 18:56:49 -0300

    ADD: Se crea views para actualizar prestamos de libros EditarPrestamoLibro

--- src/biblioteca/views.py
+++ src/biblioteca/views.py
@@ -1,12 +1,20 @@
 from .models import PrestamoLibro
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, UpdateView
 from .forms import PrestamoLibroForm
 from .models import PrestamoLibro
+from django.urls import reverse_lazy
+
 class ListarPrestamoLibro(ListView):
 	model = PrestamoLibro
-	template_name = "prestamoLibro/listar.html"
+	template_name = "biblioteca/listar.html"
 
 class CrearPrestamoLibro(CreateView):
     model = PrestamoLibro
     form_class = PrestamoLibroForm
-    template_name = "prestamolibros/crear.html"
+    template_name = "biblioteca/crear.html"
+
+class EditarPrestamoLibro(UpdateView):
+    model = PrestamoLibro
+    form_class = PrestamoLibroForm
+    template_name = "biblioteca/editar.html"
+    success_url = reverse_lazy('biblioteca:ListarPrestamoLibros') 

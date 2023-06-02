ffce59ff07ff16927eb4bca9ae6e8f9358923f0f
Joaquin Tejerina <joaquintejerina19@hotmail.com>
2023-06-02 20:11:31 -0300

    Creada la vista para el endpoint de un libro por id

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import detalle_libro
 
 app_name = "api"
 
-urlpatterns = []
+urlpatterns = [
+
+	path('libros/<int:id>',detalle_libro,name = 'detalle_libro')
+
+]
--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from django.http import JsonResponse
+from libro.models import Libro
 # Create your views here.
+
+def detalle_libro(request,id):
+	try: 
+		libro = Libro.objects.get(id = id)
+	except:
+		libro = None
+
+	if libro != None:
+		return JsonResponse({
+			'id':libro.id,
+			'titulo': libro.titulo,
+			'descripcion': libro.descripcion,
+			'autor': libro.autor.nombre
+		})
+	else:
+		return JsonResponse({})

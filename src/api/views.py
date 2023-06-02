208df6b5ebc2a1e246ccbceacec0bb8623e4073f
Dan <dantealbertomartinez@gmail.com>
2023-06-02 17:47:50 -0300

    se agrego vista:  api/listar_libro

--- src/api/views.py
+++ src/api/views.py
@@ -1,26 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from libro.models import Libro
-from autor.models import Autor
 
-"""def listar_libros(request):
+
+    
+def listar_libros(request):
     libros = Libro.objects.all()
-    print (libros)
-    data = []
-    for libro in libros:
-        data.append({
+    libros_dict = [
+        {
             'id': libro.id,
             'titulo': libro.titulo,
-            'autor': libro.autor,
-        })
-    return JsonResponse(data, safe=False)"""
-
-
-def listar_libros(request):
-  
-    libros = Libro.objects.values('id', 'titulo','autor')
-    print(libros)
-    data = {
-        'libros' : list(libros)
-    }
-    return JsonResponse(data)
+            'autor': str(libro.autor) 
+        }
+        for libro in libros
+    ]
+    return JsonResponse(libros_dict,safe=False)

95b6e70efe017f408766709dd3aa21ba0a86e316
GabrielP95 <plazagabriel1995@gmail.com>
2023-06-05 19:05:48 -0300

    Modifico el forms.py de biblioteca

--- src/biblioteca/forms.py
+++ src/biblioteca/forms.py
@@ -2,8 +2,10 @@ from django.forms import ModelForm
 from .models import PrestamoLibro, Libro, Socio, Empleado
 from django import forms
 from .validators import validar_libro_disponible
+from django.utils import timezone
 
 class PrestamoLibroForm(ModelForm):
+	fecha_prestamos = forms.DateTimeField(disabled=True, initial=timezone.now())
 	libro = forms.ModelChoiceField(queryset = Libro.objects.filter(activo = True),validators=[validar_libro_disponible])
 	socio = forms.ModelChoiceField(queryset = Socio.objects.filter(activo = True))
 	empleado = forms.ModelChoiceField(queryset = Empleado.objects.filter(activo = True))

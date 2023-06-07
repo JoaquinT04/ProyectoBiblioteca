4ae327f6f91b2baa15ac87ff7c62944be7014f05
xalvlax <sergiosalvatierra86@gmail.com>
2023-06-06 21:38:37 -0300

    ADD: Se crea url para el endpoint del listado de autores api/autores

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import listar_libros, detalle_libro
+from .views import listar_libros, detalle_libro, listar_autores
 
 app_name = "api"
 
 urlpatterns = [
 	path('libros/<int:id>',detalle_libro,name = 'detalle_libro'),
-    path('libros', listar_libros, name='listar_libros'),
+    path('libros', listar_libros, name= 'listar_libros'),
+    path('autores', listar_autores, name= 'listar_autores'),
 ]
 
 

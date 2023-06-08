7c19b53ff572bb28ae7a848d4e09d21d65b15a68
Joaquin Tejerina <joaquintejerina19@hotmail.com>
2023-06-08 13:16:37 -0300

    Documentacion de los endpoints de los modulos libro, autor, socio y empleado

--- src/api/serializer.py
+++ src/api/serializer.py
@@ -6,7 +6,7 @@ from socio.models import Socio
 class AutorGetSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Autor
-		fields = ('id','nombre','apellido','nacionalidad')
+		fields = ('id','nombre','apellido','nacionalidad','activo')
 
 
 class LibroGetSerializer(serializers.ModelSerializer):
@@ -14,14 +14,14 @@ class LibroGetSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Libro
 		#le paso el autor serializado declarado arriba
-		fields = ('id','titulo','descripcion','isbn','autor')
+		fields = ('id','titulo','descripcion','isbn','autor','activo')
 	
 class EmpleadoGetSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Empleado
-		fields = ('id','nombre','apellido','numero_legajo')
+		fields = ('id','nombre','apellido','numero_legajo','activo')
 
 class SocioGetSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Socio
-		fields = ('id','apellido','nombre','fecha_nacimiento')
+		fields = ('id','apellido','nombre','fecha_nacimiento','activo')

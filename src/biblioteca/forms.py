e65bd62d64b56d014d184cae2c24c962470670fe
GabrielP95 <plazagabriel1995@gmail.com>
2023-07-04 00:49:13 -0300

    agrego widgets a los campos que requieren seleccion de opciones

--- src/biblioteca/forms.py
+++ src/biblioteca/forms.py
@@ -3,9 +3,9 @@ from .models import PrestamoLibro, Libro, Socio, Empleado
 from django import forms
 
 class PrestamoLibroForm(ModelForm):
-	libro = forms.ModelChoiceField(queryset = Libro.objects.filter(activo = True))
-	socio = forms.ModelChoiceField(queryset = Socio.objects.filter(activo = True))
-	empleado = forms.ModelChoiceField(queryset = Empleado.objects.filter(activo = True))
+	libro = forms.ModelChoiceField(queryset = Libro.objects.filter(activo = True),widget=forms.Select(attrs={'class':'form-control'}))
+	socio = forms.ModelChoiceField(queryset = Socio.objects.filter(activo = True),widget=forms.Select(attrs={'class':'form-control'}))
+	empleado = forms.ModelChoiceField(queryset = Empleado.objects.filter(activo = True),widget=forms.Select(attrs={'class':'form-control'}))
 	class Meta():
 		model = PrestamoLibro
 		fields =["libro","socio","empleado"]
--- src/libro/forms.py
+++ src/libro/forms.py
@@ -4,7 +4,7 @@ from .validators import isbnValidator
 from django import forms
 
 class LibroForm(ModelForm):
-		autor = forms.ModelChoiceField(queryset=Autor.objects.filter(activo=True))
+		autor = forms.ModelChoiceField(queryset=Autor.objects.filter(activo=True),widget=forms.Select(attrs={'class':'form-control'}))
 		isbn = forms.CharField(validators=[isbnValidator], widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Ingrese el ISBN'}))
 		class Meta:
 				model = Libro
